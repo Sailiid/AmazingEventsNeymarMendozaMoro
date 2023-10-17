00d0744a42022ff81773c4a172701df1b504544c
sailiid <neymarsaime@gmail.com>
2023-10-17 12:48:24 -0500

    agregue descripcion

--- home.js
+++ home.js
@@ -9,16 +9,19 @@ function tarjetasSprint(arrayDatos) {
    for (let events of dataSprint) {
       const tarjetas = document.createElement("div")
       tarjetas.classList.add("tarjetas")
-      tarjetas.style.width = "13rem"
-      tarjetas.style.height = "18rem"
-      tarjetas.innerHTML = ` <div class="d-flex flex-column p-1 rounded-top bg-body-secondary border border-dark">
- <img src="${events.image}" class="card-img-top rounded-top h-50" alt="museum">
+      tarjetas.style.width = "20rem"
+      tarjetas.style.height = "30rem"
+      tarjetas.innerHTML = ` <div class="d-flex flex-column p-1 rounded-top bg-body-secondary border border-light">
+ <img src="${events.image}" class=" imagen card-img-top rounded-top h-50" alt="">
  <div class="card-body text-center text-uppercase pt-3">
     <h5 class="card-title ">${events.name}</h5>
- </div>
- <div class="cuerpotarjetas">
-    <p class="card-text fs-4 text-danger"> $ ${events.price}</p>
-    <a href="./Details.html" class="btn btn-dark">Details</a>
+    </div>
+    <div class="card-body text-center fs-5>
+    <p class="card-text">${events.description}</p>
+    </div>
+ <div class="cuerpotarjetas px-4">
+    <p class="card-text fs-3 text-danger"> $ ${events.price}</p>
+    <a href="./Details.html" class="btn btn-outline-dark btn-lg">Details</a>
  </div>
  </div>`
       cardsHome.appendChild(tarjetas)
--- pastEvents.js
+++ pastEvents.js
@@ -18,16 +18,19 @@ for ( let events of dataSprint) {
     if (datos > events.date) {
         const tarjetas = document.createElement("div")
         tarjetas.classList.add("tarjetas")
-        tarjetas.style.width = "13rem"
-        tarjetas.style.height = "20rem"
-        tarjetas.innerHTML = ` <div class="d-flex flex-column p-1 rounded-top bg-danger-subtle border border-danger">
+        tarjetas.style.width = "20rem"
+        tarjetas.style.height = "30rem"
+        tarjetas.innerHTML = ` <div class="d-flex flex-column p-1 rounded-top bg-danger-subtle border-danger-subtle">
  <img src="${events.image}" class="card-img-top rounded-top h-50" alt="museum">
  <div class="card-body text-center text-uppercase pt-3">
     <h5 class="card-title ">${events.name}</h5>
  </div>
- <div class="cuerpotarjetas">
+ <div class="card-body text-center fs-5>
+    <p class="card-text">${events.description}</p>
+ </div>
+ <div class="cuerpotarjetas px-4">
     <p class="card-text fs-4 text-danger"> $ ${events.price}</p>
-    <a href="./Details.html" class="btn btn-dark">Details</a>
+    <a href="./Details.html" class="btn btn-danger">Details</a>
  </div>
  </div>`
         cardsPast.appendChild(tarjetas)
